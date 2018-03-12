46a9e9ad44e92119d9952c0bf2ec02a1a183cc55
mashiji <mashiji104@163.com>
2018-03-12 13:42:40 +0800

    Update Reverse Words in a String.cpp

--- 151/Reverse Words in a String.cpp
+++ 151/Reverse Words in a String.cpp
@@ -69,34 +69,37 @@ public:
 
 class Solution {  
 public:  
-    void reverseWords(string &s) {  
-        stack<int> word;  
-        stack<int> sentence;  
-        int i = 0;  
-          
-        while(i <= s.size()){  
-            if(i == s.size() || s[i] == ' '){  
-                if(!word.empty()){  
-                    if(!sentence.empty()){  
-                        sentence.push(' ');  
-                    }  
-                    while(!word.empty()){  
-                        sentence.push(word.top());  
-                        word.pop();  
-                    }  
-                }  
-            } else{  
-                word.push(s[i]);  
-            }  
-            i++;  
-        };  
-          
-        s.clear();  
-        while(!sentence.empty()){  
-            s.push_back(sentence.top());  
-            sentence.pop();  
-        };  
-    }  
+    void reverseWords(string &s) stack<int>word;
+	stack<int>sentence;
+	int i = 0;
+	int n = s.size();
+	while (i <=n)
+	{
+		if (i == n || s[i] == ' ')
+		{
+			if (!word.empty())
+			{
+				if (!sentence.empty())
+				 {	
+				 	sentence.push(' ');
+				 }	
+				while (!word.empty())
+			   {
+				sentence.push(word.top());
+				word.pop();
+			   }		
+			}			
+		}
+		else{
+			word.push(s[i]);
+		}
+		i++;
+	}
+	s.clear();
+	while (!sentence.empty()){
+		s.push_back(sentence.top());
+		sentence.pop();
+	}
 };  
 
  
